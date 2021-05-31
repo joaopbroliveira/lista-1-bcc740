7743d8b3727e65e349b8ebc353309de139112f53
joaopbroliveira <85127853+joaopbroliveira@users.noreply.github.com>
2021-05-31 18:22:49 -0300

    Create ex-2-letra-e.py

---
+++ ex-2-letra-e.py
@@ -0,0 +1,6 @@
+sentence = "Practice Problems to Drill List Comprehension in Your Head."
+
+sentence = sentence.split()
+
+sentence = [i.replace(".", "") for i in sentence]
+[print(i) for i in sentence if len(i)<5]
