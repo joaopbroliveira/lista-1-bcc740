0201ff245a03d9087b9d6d980f6334e2f9951281
joaopbroliveira <85127853+joaopbroliveira@users.noreply.github.com>
2021-05-31 18:57:04 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+class calculadora1: 
+    def __init__(a,x,y):
+        a.x = x 
+        a.y = y 
+    def soma(a):
+        return a.x + a.y
+    def sub(a): 
+        return a.x - a.y
+    def multi(a): 
+        return a.x * a.y 
+    def div(a):
+        return a.x / a.y 
