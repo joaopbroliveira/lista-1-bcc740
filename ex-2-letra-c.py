34e482867c759130ecabd37d2cbf9d36867a08f7
joaopbroliveira <85127853+joaopbroliveira@users.noreply.github.com>
2021-05-31 18:20:02 -0300

    Create ex-2-letra-c.py

---
+++ ex-2-letra-c.py
@@ -0,0 +1,5 @@
+sentence = "Practice Problems to Drill List Comprehension in Your Head."
+print (sentence.count(" "))
+somatorio = [ + 1 for i in sentence if i == " "]
+
+print ("Número de espaços na string: \n ",sum(somatorio))
